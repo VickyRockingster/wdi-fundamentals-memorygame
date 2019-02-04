3c02848c5807245e0a0003bc45d6c02b84cb8356
vickyrockingster <vrockingster@gmail.com>
2019-02-03 23:45:04 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,24 @@
-const cards = ['queen', 'queen', 'king', 'king']
+const cards = [
+{
+name: 'queen',
+suit: 'hearts',
+cardImage: "../images/queen-of-hearts/png"
+}, 
+{
+name: 'queen',
+suit: 'diamonds',
+cardImage: "../images/queen-of-diamonds/png"
+}, 
+{
+name: 'king',
+suit: 'hearts',
+cardImage: "../images/king-of-hearts/png"
+}, 
+{
+name: 'king',
+suit: 'diamonds',
+cardImage: "../images/king-of-diamonds/png"
+}]
 const cardsInPlay = []
 const checkForMatch = function () {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -9,8 +29,10 @@ const checkForMatch = function () {
 }
 const flipCard = function (cardId) {
 	checkForMatch()
-console.log('User flipped ' + cards[cardId])
-cardsInPlay.push(cards[cardId])
+	console.log('User flipped ' + cards[cardId].name)
+	cardsInPlay.push(cards[cardId].name)
+	console.log(cards[cardId].cardImage)
+	console.log(cards[cardId].suit)
 }
 flipCard(0)
 flipCard(2)
