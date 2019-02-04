7375749626a3ff6c841d8282479d199b21271a9a
vickyrockingster <vrockingster@gmail.com>
2019-02-03 23:01:42 -0500

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!")
+const cardOne = 'queen'
+const cardTwo = 'queen'
+const cardThree = 'king'
+const cardFour = 'king'
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
