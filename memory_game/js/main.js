b3212e322247fde66a5bb3e779ac7f03da02dd1d
vickyrockingster <vrockingster@gmail.com>
2019-02-03 23:30:02 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,16 @@
-console.log("Up and running!")
-const cardOne = 'queen'
-const cardTwo = 'queen'
-const cardThree = 'king'
-const cardFour = 'king'
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+const cards = ['queen', 'queen', 'king', 'king']
+const cardsInPlay = []
+const checkForMatch = function () {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			console.log('You found a match!')
+		} else {
+			console.log('Sorry, try again.')
+		}
+}
+const flipCard = function (cardId) {
+	checkForMatch()
+console.log('User flipped ' + cards[cardId])
+cardsInPlay.push(cards[cardId])
+}
+flipCard(0)
+flipCard(2)
